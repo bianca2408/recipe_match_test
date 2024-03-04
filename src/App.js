beb652c59fab4a2d9096c96dc9130731453686bd
bianca2408 <bianca.istrate2408@gmail.com>
2024-03-04 14:40:11 +0200

    deploy 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import './App.css';
+
+import Home from "./pages/home.jsx";
+
+
+
+import Login from "./pages/Login.jsx";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ceva
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div>
+      <Login />
+      
+        
+   </div>
+   
+
+   
   );
 }
 
